9d8affa080207ab29eb079799cf13daabe3039b8
Sagar <mis2@sagartmt.com>
2026-01-13 17:48:07 +0530

    update frotnend

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -11,6 +11,7 @@ const MyProfile = () => {
   const [loading, setLoading] = useState(true);
   const { user, token } = useAuth();
 
+  
   const [selectedProfileImg, setSelectedProfileImg] = useState(null);
   const [selectedDocumentImg, setSelectedDocumentImg] = useState(null);
   const [previewProfileImg, setPreviewProfileImg] = useState(null);
